78e9b58a0a3fba7b5f16ca5fe8204d9443394c1f
Remco Overdijk <remco@maxserv.com>
2014-11-05 15:33:29 +0100

    Made the RHEL installer version 7 compatible by using systemctl to start puppet and installing the correct repository based on architecture

--- install-puppet-agent-redhat.sh
+++ install-puppet-agent-redhat.sh
@@ -11,14 +11,14 @@ else
 		echo puppet is not yet installed, good.; 
 	fi
 fi
-MINOR=7
+MINOR=11
 if command -v lsb_release > /dev/null 2>&1; then
 			echo "installing for:";
 			lsb_release -a;
 			VERSION=`lsb_release -sr | cut -d. -f1`;
 			MINOR=`lsb_release -sr | cut -d. -f2`;
 else
-		if [ ! -f /etc/system-release-cpe ]; then	
+		if [ ! -f /etc/system-release-cpe ]; then
 			echo "Cannot determine OS release/version, not good.";
 			exit 1;
 		else
@@ -27,7 +27,7 @@ else
 		fi
 fi
 
-rpm -ivh http://yum.puppetlabs.com/el/${VERSION}/products/i386/puppetlabs-release-${VERSION}-${MINOR}.noarch.rpm
+rpm -ivh http://yum.puppetlabs.com/el/${VERSION}/products/`uname -i`/puppetlabs-release-${VERSION}-${MINOR}.noarch.rpm
 yum clean all
 yum install puppet -y
 if grep -q "84\.53\.103\.71" /etc/hosts; then
@@ -41,4 +41,8 @@ else
 	echo puppet master added to hosts file;
 fi
 puppet agent --waitforcert 60 --test
-/etc/init.d/puppet start
+if [ -f /etc/init.d/puppet ]; then
+	/etc/init.d/puppet start
+else
+	/bin/systemctl start puppet
+fi
